146d2a0f5de73083f2ae5b697e464abbcd2b41c3
Lee <YuhangLee1996@gmail.com>
2020-08-18 16:30:43 +0800

    feat: 开始实现加载器

---
+++ core/src/main/java/me/yugang/album/core/loader/AlbumLoader.kt
@@ -0,0 +1,35 @@
+package me.yugang.album.core.loader
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.os.Build
+import android.provider.MediaStore
+import androidx.loader.content.CursorLoader
+
+class AlbumLoader(context: Context) : CursorLoader(
+    context,
+    MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) PROJECTION_29 else PROJECTION,
+    null,
+    null,
+    null
+) {
+    companion object {
+        private const val BUCKET_ID = "bucket_id"
+        private const val BUCKET_DISPLAY_NAME = "bucket_display_name"
+
+        private const val DEFAULT_ORDER_BY = "datetaken DESC"
+
+        private val PROJECTION =
+            arrayOf(MediaStore.MediaColumns._ID, BUCKET_ID, BUCKET_DISPLAY_NAME)
+
+        @SuppressLint("InlinedApi")
+        private val PROJECTION_29 =
+            arrayOf(
+                MediaStore.MediaColumns._ID,
+                MediaStore.MediaColumns.BUCKET_ID,
+                MediaStore.MediaColumns.BUCKET_DISPLAY_NAME
+            )
+    }
+
+}
